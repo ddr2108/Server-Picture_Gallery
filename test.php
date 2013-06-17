e123be4ea6383b82538af92851d644160fd4e4a0
deep <deep@Deep-UbuntuServer.(none)>
2013-06-16 21:02:39 -0400

    Got folder structure to update properly

--- test.php
+++ test.php
@@ -1,7 +1,23 @@
 <?php
 
-function fileStruct($dir){ 
-    echo '<a href ="?dir=a">Link to here</a><br>';
+//Print the folder structure
+function fileStruct($dir){
+    //Get working directory of pictures
+    $rootDir = getcwd();
+    $cwd = $rootDir  .  $dir;
+
+    //Files in current directory
+    $files = scandir($cwd);
+
+    //Go through each file and create link
+    foreach ($files as $item){
+	//if file hidden, then don't show
+	if ((strpos($item, '.')===FALSE)){
+		$href = '?dir=' . $dir . '/'  . $item;  
+		echo "<a href =\"$href\">$item</a><br>";
+	}
+    }
+
 } 
 
 ?>
@@ -9,7 +25,6 @@ function fileStruct($dir){
 <html><body> 
 
 <?php 
-
 //Check if directory is set
 if (isset($_GET['dir'])) 
 	$dir=$_GET['dir']; 
@@ -18,9 +33,6 @@ else
 
 //Call fx to create links
 fileStruct($dir)
-
- 
-
 ?> 
 
 </body></html>
